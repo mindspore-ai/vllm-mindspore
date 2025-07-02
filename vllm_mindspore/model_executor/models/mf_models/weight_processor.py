4c7c41c022eac4b97cb1e34f43c9e6b64857e0ac
huangzhuo <huangzhuo17@huawei.com>
2025-07-02 16:40:23 +0800

    a8w8 dynamicquant safetensors support 310p

--- vllm_mindspore/model_executor/models/mf_models/weight_processor.py
+++ vllm_mindspore/model_executor/models/mf_models/weight_processor.py
@@ -18,8 +18,13 @@ transform huggingface safetensor.
 """
 
 import os
+import numpy as np
 from safetensors import safe_open
+import mindspore as ms
 from mindspore.communication.management import get_rank, get_group_size
+from mindformers.version_control import is_310p
+from mindformers.experimental.infer.core.utils import get_pp_world_size
+from vllm_mindspore.utils import convert_np_to_ms_dtype
 from vllm.distributed import get_tensor_model_parallel_world_size, get_tensor_model_parallel_rank
 
 class BaseWeightProcessor:
@@ -35,13 +40,31 @@ class BaseWeightProcessor:
         self.config = config
         self.network = network
         self.is_quant = is_quant
+        self.is_310 = is_310p()
+        self.pp_group_size = get_pp_world_size()
         # self.tp_group_size = get_group_size()
         self.tp_group_size = get_tensor_model_parallel_world_size()
+        self.global_rank_id = get_rank()
         self.rank_id = get_tensor_model_parallel_rank()
+        self.pp_stage = self.global_rank_id // self.tp_group_size
         self.parameter_dict = {}
         self.file_handles = {}
         self.is_split_param = self.tp_group_size > 1
-        
+
+    def get_layer_index(self, num_layers):
+        offset = self.config.model.model_config.offset
+        stage_layers = num_layers // self.pp_group_siz
+        start_layer_index = self.pp_stage * stage_layers
+        end_layer_index = start_layer_index + stage_layers
+
+        if self.pp_group_siz > 1 and num_layers % self.pp_group_siz != 0:
+            for num in range(0, self.pp_stage):
+                start_layer_index += offset[num]
+                end_layer_index += offset[num]
+            end_layer_index += offset[self.pp_stage]
+
+        return start_layer_index, end_layer_index
+
     def get_file_handles(self, filename):
         if filename not in self.file_handles:
             fp = safe_open(filename, framework="np")
@@ -60,6 +83,9 @@ class BaseWeightProcessor:
             qint4 = True
         if not is_split_param:
             np_data = sf_file.get_tensor(hf_param_name)
+            data_dtype = convert_np_to_ms_dtype(np_data)
+            if self.is_310 and data_dtype == ms.bfloat16:
+                np_data = np_data.astype(np.float32).astype(np.float16)
             return np_data, qint4
 
         np_data = sf_file.get_slice(hf_param_name)
@@ -81,6 +107,9 @@ class BaseWeightProcessor:
             split_data = np_data[:, :, start:stop]
         else:
             raise ValueError("split_axis:{} is not supported.".format(split_axis))
+        data_dtype = convert_np_to_ms_dtype(split_data)
+        if self.is_310 and data_dtype == ms.bfloat16:
+            split_data = split_data.astype(np.float32).astype(np.float16)
         return split_data, qint4
 
     def split_weight_by_rank(self, weight, split_axis=0):
