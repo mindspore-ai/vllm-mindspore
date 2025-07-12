11ccd64f240703afa7acee5dfa7a206698cad6de
huzhikun <huzhikun1@yeah.net>
2025-07-12 07:26:29 +0000

    !628 [develop][codecheck] clean code style warn Merge pull request !628 from uuhuu/0711-style-warn
    (cherry picked from commit 209919c22c5b3abeb3249abda9fbaaca8ff84e7d)

--- vllm_mindspore/worker/profile.py
+++ vllm_mindspore/worker/profile.py
@@ -15,32 +15,36 @@
 # limitations under the License.
 
 import os
-import sys
 import subprocess
-
-from vllm.logger import init_logger
+import sys
 
 from mindspore import Profiler
-from mindspore.profiler import ProfilerLevel, ProfilerActivity, AicoreMetrics
+from mindspore.profiler import ProfilerActivity, ProfilerLevel
 from mindspore.profiler.common.profiler_context import ProfilerContext
+from vllm.logger import init_logger
 
 logger = init_logger(__name__)
 
 PROFILE_ENV_NAME = "VLLM_TORCH_PROFILER_DIR"
 
+
 def shell_analyse(path):
-    subprocess.run(
-        [sys.executable, "-c", f'from mindspore import Profiler; Profiler.offline_analyse("{path}")'],
-        shell=False, check=True)
+    subprocess.run([
+        sys.executable, "-c",
+        f'from mindspore import Profiler; Profiler.offline_analyse("{path}")'
+    ],
+                   shell=False,
+                   check=True)
+
 
 class AdapterProfiler:
+
     def __init__(self, path):
         self.profiler = Profiler(
             profiler_level=ProfilerLevel.Level1,
             activities=[ProfilerActivity.CPU, ProfilerActivity.NPU],
             output_path=path,
-            start_profile=False
-        )
+            start_profile=False)
 
     def start(self):
         self.profiler.start()
@@ -50,12 +54,15 @@ class AdapterProfiler:
         path = ProfilerContext().ascend_ms_dir
         shell_analyse(path)
 
+
 def wrapper_worker_init(fun):
+
     def new_fun(*arg, **kwarg):
         # Profiler initialization during worker init triggers device setup,
         # causing init_device to fail due to duplicate configuration.
-        # To fix this, temporarily unset VLLM_TORCH_PROFILER_DIR before worker init,
-        # restore it afterward, then initialize profiler properly after worker init_device completes
+        # To fix this, temporarily unset VLLM_TORCH_PROFILER_DIR
+        # before worker init, restore it afterward,
+        # then initialize profiler properly after worker init_device completes
         profile_output_path = os.getenv(PROFILE_ENV_NAME, "")
         if profile_output_path:
             del os.environ[PROFILE_ENV_NAME]
@@ -64,21 +71,26 @@ def wrapper_worker_init(fun):
 
         if profile_output_path:
             os.environ[PROFILE_ENV_NAME] = profile_output_path
+
     return new_fun
 
+
 def wrapper_worker_init_device(fun):
+
     def new_fun(*arg, **kwarg):
         fun(*arg, **kwarg)
 
-        # The actual profiler initialization is performed after the worker.init_device() method,
+        # The actual profiler initialization is performed
+        # after the worker.init_device() method,
         # based on the VLLM_TORCH_PROFILER_DIR environment variable.
+        # NOTE: ref to `Worker.__init__` in vllm
         self = arg[0]
         profile_output_path = os.getenv(PROFILE_ENV_NAME, "")
         if profile_output_path:
-            logger.info(f"Profiling enabled. Traces will be saved to: {profile_output_path}")
+            logger.info("Profiling enabled. Traces will be saved to: %s",
+                        profile_output_path)
             self.profiler = AdapterProfiler(profile_output_path)
         else:
             self.profiler = None
+
     return new_fun
-
-
