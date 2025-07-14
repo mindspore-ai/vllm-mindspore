b45c1fbae77a825038a55401a25ff6f64e0e9b65
r1chardf1d0 <xiaruijie@huawei.com>
2025-07-14 15:58:06 +0800

    clean code

--- vllm_mindspore/worker/worker.py
+++ vllm_mindspore/worker/worker.py
@@ -15,80 +15,90 @@
 # limitations under the License.
 """Adapted functions for mindspore in Worker."""
 
-import gc
-import os
 import math
-from typing import Tuple, Optional
 
 import torch
-
-from vllm.config import VllmConfig
-from vllm.distributed import (
-    ensure_kv_transfer_initialized,
-    ensure_model_parallel_initialized,
-    init_distributed_environment,
-    set_custom_all_reduce,
-)
-
 from vllm.logger import init_logger
+from vllm.model_executor import set_random_seed
+from vllm.sampling_params import SamplingParams
+from vllm.sequence import SequenceGroupMetadata
 
 from vllm_mindspore.utils import get_valid_dtype
-from vllm.model_executor import set_random_seed
-from vllm.sequence import SequenceGroupMetadata
-from vllm.sampling_params import SamplingParams
-
 
 logger = init_logger(__name__)
 
 
-def _prepare_input_for_warmup(model_config, model_runner, cache_engine, is_prefill, is_mtp_model=False):
+def _prepare_input_for_warmup(model_config,
+                              model_runner,
+                              cache_engine,
+                              is_prefill,
+                              is_mtp_model=False):
     bs = 1
-    seq_len = model_runner.scheduler_config.max_num_batched_tokens if is_prefill else 1
-    dummy_data = model_runner.input_registry.dummy_data_for_profiling(model_config, seq_len, model_runner.mm_registry)
-    block_tables = [i for i in range(math.ceil(seq_len / cache_engine.block_size))]
+    seq_len = model_runner.scheduler_config.max_num_batched_tokens \
+        if is_prefill else 1
+    dummy_data = model_runner.input_registry.dummy_data_for_profiling(
+        model_config, seq_len, model_runner.mm_registry)
+    block_tables_num = [
+        i for i in range(math.ceil(seq_len / cache_engine.block_size))
+    ]
     seqs = [
         SequenceGroupMetadata(
             request_id=str(idx),
             is_prompt=is_prefill,
             seq_data={idx: dummy_data.seq_data},
             sampling_params=SamplingParams(),
-            block_tables={idx: block_tables},
+            block_tables={idx: block_tables_num},
             lora_request=None,
             multi_modal_data=None,
             multi_modal_placeholders=None,
-        )
-        for idx in range(bs)
+        ) for idx in range(bs)
     ]
 
     model_input = model_runner.prepare_model_input(seqs)
     block_tables = model_input.attn_metadata.block_tables
     if block_tables is not None and block_tables.numel() <= 0:
-        model_input.attn_metadata.block_tables = torch.zeros((1, 1), dtype=torch.int32)
+        model_input.attn_metadata.block_tables = torch.zeros((1, 1),
+                                                             dtype=torch.int32)
 
-    previous_hidden_states = None if not is_mtp_model else \
-        torch.ones([bs, seq_len, model_config.get_hidden_size()], dtype=get_valid_dtype(model_config.dtype))
+    previous_hidden_states = None if not is_mtp_model else torch.ones(
+        [bs, seq_len, model_config.get_hidden_size()],
+        dtype=get_valid_dtype(model_config.dtype))
     return model_input, previous_hidden_states
 
 
 def _warm_up_model(self) -> None:
-    # cache_engine is a list with length equal to the size of pipeline-parallel, and only pp=1 is supported.
+    # cache_engine is a list with length equal to the size of
+    # pipeline-parallel, and only pp=1 is supported.
     kv_cache = self.cache_engine[0].gpu_cache
-    is_mtp_model = self.speculative_config is not None and self.model_config.hf_config.model_type == "deepseek_mtp"
+    is_mtp_model = self.speculative_config is not None and \
+        self.model_config.hf_config.model_type == "deepseek_mtp"
     if is_mtp_model:
         # prefill mtp model
-        model_input, previous_hidden_states = _prepare_input_for_warmup(self.model_config, self.model_runner,
-                                                                        self.cache_engine[0], True, is_mtp_model)
-        self.model_runner.execute_model(model_input, kv_cache, None, previous_hidden_states=previous_hidden_states)
+        model_input, previous_hidden_states = _prepare_input_for_warmup(
+            self.model_config, self.model_runner, self.cache_engine[0], True,
+            is_mtp_model)
+        self.model_runner.execute_model(
+            model_input,
+            kv_cache,
+            None,
+            previous_hidden_states=previous_hidden_states)
 
     # warmup for decode
     if self.vllm_config.scheduler_config.is_multi_step:
-        model_input, _ = _prepare_input_for_warmup(self.model_config, self.model_runner._base_model_runner,
-                                                   self.cache_engine[0], False)
-        self.model_runner._base_model_runner.execute_model(model_input, kv_cache, None)
+        model_input, _ = _prepare_input_for_warmup(
+            self.model_config, self.model_runner._base_model_runner,
+            self.cache_engine[0], False)
+        self.model_runner._base_model_runner.execute_model(
+            model_input, kv_cache, None)
     else:
-        model_input, previous_hidden_states = _prepare_input_for_warmup(self.model_config, self.model_runner,
-                                                                        self.cache_engine[0], False, is_mtp_model)
-        self.model_runner.execute_model(model_input, kv_cache, None, previous_hidden_states=previous_hidden_states)
+        model_input, previous_hidden_states = _prepare_input_for_warmup(
+            self.model_config, self.model_runner, self.cache_engine[0], False,
+            is_mtp_model)
+        self.model_runner.execute_model(
+            model_input,
+            kv_cache,
+            None,
+            previous_hidden_states=previous_hidden_states)
 
     torch.cuda.synchronize()
 
