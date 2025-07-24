5ae095aad0e87ae26b380360667a4a9aa4f41722
moran <moran2@huawei.com>
2025-07-24 10:15:27 +0800

    fix all codecheck

--- vllm_mindspore/v1/sample/ops/penalties.py
+++ vllm_mindspore/v1/sample/ops/penalties.py
@@ -18,13 +18,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List
-
 import torch
 from vllm.utils import is_pin_memory_available, make_tensor_with_pad
 
 
-def _convert_to_tensors(output_token_ids: List[List[int]], vocab_size: int,
+def _convert_to_tensors(output_token_ids: list[list[int]], vocab_size: int,
                         device: torch.device) -> torch.Tensor:
     """
     Convert the different list data structures to tensors.
