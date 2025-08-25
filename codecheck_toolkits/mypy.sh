5fc7c71777d37b15fb0caef9eee6cf9e35bc0d79
moran <moran2@huawei.com>
2025-08-25 09:37:22 +0800

    fix mypy codecheck

--- codecheck_toolkits/mypy.sh
+++ codecheck_toolkits/mypy.sh
@@ -5,22 +5,21 @@
 PYTHON_VERSION=${1:-local}
 
 run_mypy() {
-    mypy --python-version "${PYTHON_VERSION}" "$@"
+    if [ -z "$1" ]; then
+      mypy --python-version "${PYTHON_VERSION}" "$@"
+      return
+    fi
+    mypy --follow-imports skip --python-version "${PYTHON_VERSION}" "$@"
 }
 
 run_mypy
 run_mypy tests/st
 run_mypy vllm_mindspore/attention
-run_mypy vllm_mindspore/compilation
 run_mypy vllm_mindspore/distributed
 run_mypy vllm_mindspore/engine
 run_mypy vllm_mindspore/executor
 run_mypy vllm_mindspore/inputs
 run_mypy vllm_mindspore/lora
 run_mypy vllm_mindspore/model_executor
-run_mypy vllm_mindspore/plugins
-run_mypy vllm_mindspore/prompt_adapter
-run_mypy vllm_mindspore/spec_decode
 run_mypy vllm_mindspore/worker
 run_mypy vllm_mindspore/v1
-
