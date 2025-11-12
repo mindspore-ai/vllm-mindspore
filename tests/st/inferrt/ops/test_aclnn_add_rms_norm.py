6e7a2464e83f7d5a8f3f4cee340223bb9915a4da
YzLi <liyizhan2@huawei.com>
2025-11-12 15:07:19 +0800

    add rms_norm, add_rms_norm op

---
+++ tests/st/inferrt/ops/test_aclnn_add_rms_norm.py
@@ -0,0 +1,85 @@
+import pytest
+import numpy as np
+import torch
+
+from tests.mark_utils import arg_mark
+from tests.ops_utils import AssertRtolEqual
+from mrt.torch import backend
+
+
+def op_func(x1, x2, gamma, epsilon=1e-6):
+    """op function for add_rms_norm"""
+    ori_dtype = x1.dtype
+    x = x1 + x2
+    if ori_dtype == np.float16:
+        x = x.astype(np.float32)
+    variance = np.mean(np.power(x, 2), axis=-1, keepdims=True)
+    std = np.sqrt(variance + epsilon)
+    rstd = np.divide(1, std)
+    result_mid = x * rstd
+    result = result_mid * gamma
+    if ori_dtype == np.float16:
+        x = x.astype(np.float16)
+        result = result.astype(np.float16)
+    return result, rstd, x
+
+
+def get_op_func_compiled():
+    def custom_op_func(x1, x2, gamma, epsilon=1e-6):
+        return torch.ops.npu.npu_add_rms_norm(x1, x2, gamma, epsilon)
+    return torch.compile(custom_op_func, backend=backend)
+
+
+def add_rms_norm_forward(dtype, op_func_compiled):
+    """
+    add_rms_norm forward function
+    Args:
+        dtype: The data type of the input.
+        op_func_compiled: The compiled op function.
+    """
+    if dtype == np.float16:
+        prec = 0.001
+    else:
+        prec = 0.0001
+
+    cpu_input0 = np.random.uniform(0, 100, [1024, 12288]).astype(dtype)
+    cpu_input1 = np.random.uniform(0, 100, [1024, 12288]).astype(dtype)
+    cpu_input2 = np.random.uniform(0, 100, [12288]).astype(dtype)
+    npu_input0 = torch.from_numpy(cpu_input0).npu()
+    npu_input1 = torch.from_numpy(cpu_input1).npu()
+    npu_input2 = torch.from_numpy(cpu_input2).npu()
+
+    cpu_output0, cpu_output1, cpu_output2 = op_func(cpu_input0, cpu_input1, cpu_input2)
+    npu_output0, npu_output1, npu_output2 = [npu_output.detach().cpu().numpy() for npu_output in op_func_compiled(npu_input0, npu_input1, npu_input2)]
+
+    AssertRtolEqual(cpu_output0, npu_output0, prec)
+    AssertRtolEqual(cpu_output1, npu_output1, prec)
+    AssertRtolEqual(cpu_output2, npu_output2, prec)
+
+
+@arg_mark(plat_marks=["platform_ascend"], level_mark="level0", card_mark="onecard", essential_mark="essential")
+@pytest.mark.parametrize("pipeline", (True, False))
+def test_add_rms_norm_fp32(pipeline, monkeypatch):
+    """
+    Feature: Test aclnn add_rms_norm
+    Description: Test aclnn add_rms_norm with fp32 inputs
+    Expectation: The result is correct
+    """
+    if pipeline:
+        monkeypatch.setenv("MRT_ENABLE_PIPELINE", "on")
+    op_func_compiled = get_op_func_compiled()
+    add_rms_norm_forward(np.float32, op_func_compiled)
+
+
+@arg_mark(plat_marks=["platform_ascend"], level_mark="level0", card_mark="onecard", essential_mark="essential")
+@pytest.mark.parametrize("pipeline", (True, False))
+def test_add_rms_norm_fp16(pipeline, monkeypatch):
+    """
+    Feature: Test aclnn add_rms_norm
+    Description: Test aclnn add_rms_norm with fp16 inputs
+    Expectation: The result is correct
+    """
+    if pipeline:
+        monkeypatch.setenv("MRT_ENABLE_PIPELINE", "on")
+    op_func_compiled = get_op_func_compiled()
+    add_rms_norm_forward(np.float16, op_func_compiled)
---
+++ tests/st/inferrt/ops/test_aclnn_rms_norm.py
@@ -0,0 +1,72 @@
+import pytest
+import numpy as np
+import torch
+
+from tests.mark_utils import arg_mark
+from tests.ops_utils import AssertRtolEqual
+from mrt.torch import backend
+
+
+def op_func(x, gamma, epsilon = 1e-6):
+    x_fp32 = np.array(x, dtype=np.float32)
+    gamma_fp32 = np.array(gamma, dtype=np.float32)
+
+    variance = np.mean(np.power(x_fp32, 2), axis=-1, keepdims=True)
+    std = np.sqrt(variance + epsilon)
+    rstd = 1 / std
+    result_mid = x_fp32 * rstd
+    result_fp32 = result_mid * gamma_fp32
+
+    result = np.array(result_fp32, dtype=x.dtype)
+
+    return result, rstd
+
+
+def get_op_func_compiled():
+    def custom_op_func(x, gamma, epsilon=1e-6):
+        return torch.ops.npu.npu_rms_norm(x, gamma, epsilon)
+    return torch.compile(custom_op_func, backend=backend)
+
+
+@arg_mark(plat_marks=["platform_ascend"], level_mark="level0", card_mark="onecard", essential_mark="essential")
+@pytest.mark.parametrize("pipeline", (True, False))
+def test_rms_norm(pipeline, monkeypatch):
+    """
+    Feature: Test aclnn rms_norm
+    Description: Test aclnn rms_norm with fp32 inputs
+    Expectation: The result is correct
+    """
+    if pipeline:
+        monkeypatch.setenv("MRT_ENABLE_PIPELINE", "on")
+    cpu_input0 = np.random.uniform(0, 100, [1024, 12288]).astype(np.float32)
+    cpu_input1 = np.random.uniform(0, 100, [12288]).astype(np.float32)
+    npu_input0 = torch.from_numpy(cpu_input0).npu()
+    npu_input1 = torch.from_numpy(cpu_input1).npu()
+
+    cpu_output0, cpu_output1 = op_func(cpu_input0, cpu_input1)
+    op_func_compiled = get_op_func_compiled()
+    npu_output0, npu_output1 = [npu_output.detach().cpu().numpy() for npu_output in op_func_compiled(npu_input0, npu_input1)]
+    AssertRtolEqual(cpu_output0, npu_output0)
+    AssertRtolEqual(cpu_output1, npu_output1)
+
+
+@arg_mark(plat_marks=["platform_ascend"], level_mark="level0", card_mark="onecard", essential_mark="essential")
+@pytest.mark.parametrize("pipeline", (True, False))
+def test_rms_norm_mix_dtype(pipeline, monkeypatch):
+    """
+    Feature: Test aclnn rms_norm
+    Description: Test aclnn rms_norm with mixed float16 and float32 inputs
+    Expectation: The result is correct
+    """
+    if pipeline:
+        monkeypatch.setenv("MRT_ENABLE_PIPELINE", "on")
+    cpu_input0 = np.random.uniform(0, 100, [1024, 12288]).astype(np.float16)
+    cpu_input1 = np.random.uniform(0, 100, [12288]).astype(np.float32)
+    npu_input0 = torch.from_numpy(cpu_input0).npu()
+    npu_input1 = torch.from_numpy(cpu_input1).npu()
+
+    cpu_output0, cpu_output1 = op_func(cpu_input0, cpu_input1)
+    op_func_compiled = get_op_func_compiled()
+    npu_output0, npu_output1 = [npu_output.detach().cpu().numpy() for npu_output in op_func_compiled(npu_input0, npu_input1)]
+    AssertRtolEqual(cpu_output0, npu_output0)
+    AssertRtolEqual(cpu_output1, npu_output1)
