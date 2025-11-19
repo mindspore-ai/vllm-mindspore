ee2d4da53cfe3ac67d90e26cac3d5f931147823a
zhanghanLeo <zhanghan50@hisilicon.com>
2025-11-19 11:27:15 +0800

    [OpsSupport]:embedding ops

---
+++ tests/st/inferrt/ops/test_aclnn_embedding.py
@@ -0,0 +1,53 @@
+import pytest
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from tests.mark_utils import arg_mark
+from tests.ops_utils import AssertRtolEqual
+from mrt.torch import backend
+
+
+def op_func(indices, weight):
+    out = F.embedding(indices, weight)
+    return out
+
+
+def get_op_func_compiled():
+    def custom_op_func(indices, weight):
+        return F.embedding(indices, weight)
+    return torch.compile(custom_op_func, backend=backend)
+
+
+@arg_mark(plat_marks=["platform_ascend"], level_mark="level0", card_mark="onecard", essential_mark="essential")
+@pytest.mark.parametrize("pipeline", (True, False))
+@pytest.mark.parametrize("dtype", (torch.float32, torch.float16))
+def test_embedding(pipeline, monkeypatch, dtype):
+    """
+    Feature: Test aclnn embedding
+    Description: Test aclnn embedding with fp32/fp16 inputs
+    Expectation: The result is correct
+    """
+    if pipeline:
+        monkeypatch.setenv("MRT_ENABLE_PIPELINE", "on")
+    cpu_weight_torch = torch.from_numpy(np.random.rand(10, 3).astype(np.float32)).to(dtype)
+    cpu_indices = torch.from_numpy(np.array([[1, 2, 4, 5], [4, 3, 2, 9]]))
+
+    npu_weight_torch = cpu_weight_torch.npu()
+    npu_indices = cpu_indices.npu()
+
+    cpu_output0 = op_func(cpu_indices, cpu_weight_torch)
+    op_func_compiled = get_op_func_compiled()
+    npu_output0 = [npu_output.detach().cpu().numpy() for npu_output in op_func_compiled(npu_indices, npu_weight_torch)]
+    AssertRtolEqual(cpu_output0, npu_output0)
+
+    cpu_weight_torch = torch.from_numpy(np.random.rand(20, 4).astype(np.float32)).to(dtype)
+    cpu_indices = torch.from_numpy(np.array([[1, 2, 4, 5], [4, 3, 2, 9]]))
+
+    npu_weight_torch = cpu_weight_torch.npu()
+    npu_indices = cpu_indices.npu()
+
+    cpu_output0 = op_func(cpu_indices, cpu_weight_torch)
+    op_func_compiled = get_op_func_compiled()
+    npu_output0 = [npu_output.detach().cpu().numpy() for npu_output in op_func_compiled(npu_indices, npu_weight_torch)]
+    AssertRtolEqual(cpu_output0, npu_output0)
