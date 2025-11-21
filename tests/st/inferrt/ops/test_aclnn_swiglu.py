d58249ce02177e8f000974ee39ae592a082c3e45
zhanghanLeo <zhanghan50@hisilicon.com>
2025-11-21 19:44:06 +0800

    [OpsSupport]:swiglu

---
+++ tests/st/inferrt/ops/test_aclnn_swiglu.py
@@ -0,0 +1,44 @@
+import pytest
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from tests.mark_utils import arg_mark
+from tests.ops_utils import AssertRtolEqual
+from mrt.torch import backend
+
+
+def op_func(input_self_tensor, dim=-1):
+    x = torch.chunk(input_self_tensor, 2, dim=dim)
+    self_tensor = x[0].type(torch.float32)
+    other = x[1].type(torch.float32)
+    output = F.silu(self_tensor) * other
+    return output.type(torch.bfloat16)
+
+def get_op_func_compiled():
+    def custom_op_func(input_self_tensor, dim=-1):
+        return torch.ops.npu.npu_swiglu(input_self_tensor, dim)
+    return torch.compile(custom_op_func, backend=backend)
+
+
+@arg_mark(plat_marks=["platform_ascend"], level_mark="level0", card_mark="onecard", essential_mark="essential")
+@pytest.mark.parametrize("pipeline", (True, False))
+@pytest.mark.parametrize("shape", [[8192, 1, 3904*2], [32, 1024]])
+def test_swiglu(pipeline, monkeypatch, shape):
+    """
+    Feature: Test aclnn swiglu
+    Description: Test aclnn swiglu with bf16 inputs
+    Expectation: The result is correct
+    """
+    if pipeline:
+        monkeypatch.setenv("MRT_ENABLE_PIPELINE", "on")
+
+    self_tensor = torch.rand(shape, dtype=torch.bfloat16)
+    dim = -1
+    self_tensor_npu = self_tensor.npu()
+
+    cpu_output = op_func(self_tensor, dim=dim)
+    op_func_compiled = get_op_func_compiled()
+    npu_output0 = op_func_compiled(self_tensor_npu, dim=dim)
+    npu_output = npu_output0.detach().cpu()
+    AssertRtolEqual(cpu_output, npu_output)
